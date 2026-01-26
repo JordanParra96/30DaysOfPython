b36c81cf3031297c6c6e50a9974b9f1d4fe00cf1
Jordan Parra <jordan.parra@coastalcloud.us>
2026-01-25 22:23:50 -0500

    day5 exercises

---
+++ day_5/lists.py
@@ -0,0 +1,6 @@
+empty_list = []
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print('length of numbers list:', len(numbers))
+print('first element of numbers list:', numbers[0])
+print('last element of numbers list:', numbers[-1])
+print('element in the middle of numbers list:', numbers[len(numbers) // 2])
