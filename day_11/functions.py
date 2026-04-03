ff26c0c3d455e91205809da85a396e28764e55ee
Jordan Parra <jordan.parra@coastalcloud.us>
2026-04-03 18:39:48 -0500

    Pylint issue had been fixed

--- day_11/functions.py
+++ day_11/functions.py
@@ -203,7 +203,7 @@ def factorial(n):
     """Returns the factorial of a number."""
     if n < 0:
         return 'Factorial is not defined for negative numbers.'
-    if n == 0 or n == 1:
+    if n in (0, 1):
         return 1
     result = 1
     for i in range(2, n + 1):
