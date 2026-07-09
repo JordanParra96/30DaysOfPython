f0ee310809c673f3bdc3d127d1daadf64057f473
Jordan Parra <jordanstevenparratorres@gmail.com>
2026-07-08 22:45:03 -0500

    Pyhon datetime exercise 1

---
+++ day_16/python_datetime.py
@@ -0,0 +1,22 @@
+"""Day 16: 30 Days of python programming"""
+
+# Exercise 1: Get the current date and time
+from datetime import datetime
+
+CURRENT_DAY = datetime.now().day
+print(f"Current day: {CURRENT_DAY}")
+
+CURRENT_MONTH = datetime.now().month
+print(f"Current month: {CURRENT_MONTH}")
+
+CURRENT_YEAR = datetime.now().year
+print(f"Current year: {CURRENT_YEAR}")
+
+CURRENT_HOUR = datetime.now().hour
+print(f"Current hour: {CURRENT_HOUR}")
+
+CURRENT_MINUTE = datetime.now().minute
+print(f"Current minute: {CURRENT_MINUTE}")
+
+CURRENT_TIMESTAMP = datetime.now().timestamp()
+print(f"Current timestamp: {CURRENT_TIMESTAMP}")
