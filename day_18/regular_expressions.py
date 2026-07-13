0a16820e9cb0a72ad1cf611f877bff543673d95f
Jordan Parra <jordanstevenparratorres@gmail.com>
2026-07-13 17:13:04 -0500

    Add split example to demonstrate string splitting functionality

--- day_18/regular_expressions.py
+++ day_18/regular_expressions.py
@@ -51,3 +51,10 @@ D%o%es thi%s m%ot%iv%a%te %y%o%u to b%e a t%e%a%cher?"""
 
 SUB_RESULT = re.sub("%", "", SUB_TEXT)
 print(SUB_RESULT)
+
+# Split example
+SPLIT_TXT = """I am teacher and  I love teaching.
+There is nothing as rewarding as educating and empowering people.
+I found teaching more interesting than any other jobs.
+Does this motivate you to be a teacher?"""
+print(re.split("\n", SPLIT_TXT))  # splitting using \n - end of line symbol
